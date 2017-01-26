d3694f0313907a966e7b6a5baecbf5a84c7d3634
brownl13 <brownl13@nku.edu>
2017-01-26 16:53:42 -0500

    updated UI

--- app/src/main/java/com/example/lrb13/cipher/MainActivity.java
+++ app/src/main/java/com/example/lrb13/cipher/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.lrb13.cipher;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.widget.Spinner;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner functionList;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        functionList = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.split_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        functionList.setAdapter(adapter);
+
     }
 }
