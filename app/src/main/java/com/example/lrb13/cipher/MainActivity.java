87706fa8e2e4596276073cc312035d6c0f33c7bc
brownl13 <brownl13@nku.edu>
2017-01-31 19:32:45 -0500

    added scroll bars

--- app/src/main/java/com/example/lrb13/cipher/MainActivity.java
+++ app/src/main/java/com/example/lrb13/cipher/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.lrb13.cipher;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ActionMenuView;
@@ -11,17 +12,25 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
 import android.widget.Spinner;
+import android.widget.EditText;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 
 public class MainActivity extends Activity {
 
     Spinner functionList;
     TextView output;
+
     int position;
-    int i=0;
-
-
+    int i = 0;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
@@ -29,6 +38,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         output = (TextView) findViewById(R.id.textView);
+
         functionList = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.split_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -60,28 +70,65 @@ public class MainActivity extends Activity {
         //doFunction(position);
 
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
     //@Override
-    public void doFunction (int p)
-    {
-        if(p==0)
-        {
+    public void doFunction(int p) {
+        if (p == 0) {
 
             output.setText("You selected Joey's Function");
-        }
-        else if(p==1)
-        {
+        } else if (p == 1) {
             output.setText("You selected Chris's Function");
-        }
-        else if(p==2)
-        {
+        } else if (p == 2) {
             output.setText("You selected Mate's Function");
         }
 
     }
 
-    public void RunOnClick(View v)
-    {
+    public void RunOnClick(View v) {
         output.setText("Run Button works");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.lrb13.cipher/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.lrb13.cipher/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
