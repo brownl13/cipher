62617b87897f9bffc788dc2c8e30511ebc6e2847
brownl13 <brownl13@nku.edu>
2017-02-07 17:13:09 -0500

    joey function 1 working

---
+++ app/src/main/java/Ufun.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Ufun{
+//1-26
+public final ArrayList<Integer> uZcon(String c){
+        c = c.toLowerCase(); //to lower
+        c = c.replaceAll(" ","");
+        ArrayList n = new ArrayList<Integer>();
+        char[] cArray = c.toCharArray(); // to char array
+
+        //loop through char array and convert letters into #
+        for(int i = 0; i < cArray.length; i++){
+        //conversion equation and add to arraylist
+        n.add((int)cArray[i] - (int)'a' + 1);
+        }
+        return n;
+
+
+        }
+//0-25
+public final ArrayList<Integer> uOcon(String c){
+        c = c.toLowerCase(); //to lower
+        c = c.replaceAll(" ","");
+        ArrayList n = new ArrayList<Integer>();
+        char[] cArray = c.toCharArray(); // to char array
+
+        //loop through char array and convert letters into #
+        for(int i = 0; i < cArray.length; i++){
+        //conversion equation and add to arraylist
+        n.add((int)cArray[i] - (int)'a');
+        }
+        return n;
+        }
+
+
+        }
